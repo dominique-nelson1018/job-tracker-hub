84210f887694f187e3c44c8af444f04043dd2376
Dominique Nelson <donelson@flvs.net>
2025-09-02 17:32:08 -0400

    Added Hero section to landing page

---
+++ src/app/components/JobForm.tsx
@@ -0,0 +1,14 @@
+export default function JobForm() {
+  return (
+    <form>
+        <h2>Job Form</h2>  
+
+        <label>Company</label>
+        <input type="text" />
+
+        <button type="submit">Save Job</button>   
+    </form>
+
+  );
+
+}
